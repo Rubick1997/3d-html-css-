2f90e9ba1cbdfb73933cd0b07afe9f65a0f71c32
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro-2.local>
2022-02-07 15:57:40 -0800

    project is ready

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const showSlier = new Swiper(".showcase-carousel", {
+    loop: true,
+    slidesPerView: 3,
+    speed: 1800,
+    centeredSlides: true,
+    navigation: {
+      nextEl: ".showcase-navigation__next",
+      prevEl: ".showcase-navigation__prev",
+    },
+  });
   document.querySelector("video").playbackRate = 2;
 });
